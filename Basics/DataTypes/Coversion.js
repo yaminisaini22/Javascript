a96a325a60034691d245bc872ccf9e9361f4e744
yaminisaini22 <yaminisaini123@gmail.com>
2024-07-01 20:11:14 +0530

    conversion

---
+++ Basics/DataTypes/Coversion.js
@@ -0,0 +1,17 @@
+let a = 33;
+console.log(typeof a);
+// conversion we use Capital letter for eg: Number instaed of number
+
+let string = String(a);
+console.log(string, typeof (string))
+
+let b = "22abc"
+console.log(typeof b)
+
+let number = Number(b); // but we can see it is not a number
+console.log(number, typeof(number));
+
+let c = null
+console.log(typeof null);
+let string2 = String(c);
+console.log(string2, typeof(string))
